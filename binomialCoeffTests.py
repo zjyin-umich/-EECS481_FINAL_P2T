8044151ea6ac4f544bb51c5d4803e2c1b8dfe66e
Natalia Sánchez <sanchnat@umich.edu>
2020-12-07 15:49:02 -0500

    Final touches.

--- binomialCoeffTests.py
+++ binomialCoeffTests.py
@@ -27,9 +27,39 @@ def binomialCoeff(n , k):
 
 
 # This function tests the binomial coefficient function by generating a random int for the value of n and k, calculating the binomial coefficient of those values, and asserting that the function calculated the same value.
-def test_binomialCoeff():
-    n = random.randint(0, 10000)
-    k = random.randint(0,n)
+def test_1():
+    n = 10
+    k = 5
 
 
-    assert(binomialCoeff(n,k) == math.comb(n,k))
+    assert(binomialCoeff(n,k) == 252)
+
+def test_2():
+    n = 234
+    k = 40
+
+    assert(binomialCoeff(n,k) == 2090382965619538776890565879870798163571167815)
+
+def test_3():
+    n = 399
+    k = 78
+
+    assert(binomialCoeff(n,k) == 2081141624878121436255182369875518249771590196600744911784261235252057494896885774670)
+
+def test_4():
+    n = 234
+    k = 200
+
+    assert(binomialCoeff(n,k) == 97364614941709388140124964830028652656819)
+
+def test_5():
+    n = 20
+    k = 8
+
+    assert(binomialCoeff(n,k) == 125970)
+
+def test_6():
+    n = 15
+    k = 25
+
+    assert(binomialCoeff(n,k) == 0)
